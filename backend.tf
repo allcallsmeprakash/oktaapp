a264c43269cf30993302f3e3f8f9b076a7d1661c
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-06 02:35:16 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "training-usecases"
-    key    = "hello-world-app/terraform.tfstate"
+    key    = "okta/terraform.tfstate"
     region = "us-east-1"
   }
 }
