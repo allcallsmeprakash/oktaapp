55f310e135954df51c51bb7d69632b8b4c47e887
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-06 03:04:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -113,3 +113,20 @@ resource "aws_cloudfront_distribution" "website_distribution" {
   }
 }
 
+# Auth0 Application Configuration
+resource "auth0_client" "hello_world_app" {
+  name            = "HelloWorldApp"
+  app_type        = "regular_web"
+  oidc_conformant = true
+
+  callbacks            = ["https://${aws_cloudfront_distribution.website_distribution.domain_name}/index.html"]
+  allowed_logout_urls  = ["https://${aws_cloudfront_distribution.website_distribution.domain_name}/logout"]
+
+  grant_types = [
+    "authorization_code",
+    "implicit",
+    "refresh_token"
+  ]
+}
+
+
